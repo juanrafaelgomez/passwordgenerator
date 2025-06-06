2145ac0696bf7389859143820de07413bfe2712b
Juan Gomez <juanrafaelgomez@gmail.com>
2025-06-05 22:01:27 -0600

    Input checkers were added before procedure call.

--- passwordGenerator.pas
+++ passwordGenerator.pas
@@ -1,26 +1,32 @@
+{$mode fpc}
 program  passwordgenerator;
 
-uses crt;
+uses crt, sysutils, character; 
 
 var
-   plength  : integer;
-   number   : char;
-   special  : char;
+   plength   : string;
+   number    : char;
+   special   : char;
+   i	     : integer;
+   isanumber : boolean;
+   nlength   : integer;
+   error     : integer;
    
 procedure passwordgenerator(plenght : integer; number : char; special : char);
-{generates a password based on the plength requested.}
+{generates a password based on lenght, special and numbers.}
 			    
 var
    dictionary : array of char;
-   selection  : string;
+   { selection  : string; }
    password   : string;
    i	      : integer;
 
 begin
-   writeln('The procedure parameters are: plength: ', plength, ' number: ', number, ' special: ', special);
+   writeln('The procedure parameters are: length: ', plength, ' number: ', number, ' special: ', special);
    randomize;
    dictionary := '';
    password := '';
+   isanumber := false;
    { selection := ''; }
 
    if (number = 'Y') and (special = 'Y') then
@@ -32,7 +38,7 @@ begin
   else
    dictionary := 'abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNOPQSTUVWXYZ';
 
-   {'length' is a Pascal function that returns the size of an array}
+   
    { for i := 0 to length(dictionary) do }
    {    begin }
    {    selection := selection + dictionary[i] }
@@ -47,9 +53,9 @@ begin
 	 letter to a number. at the end, the resulting number is
 	 converted back to a character}
 	 { password := password + char(random(26) + ord('a')); }
-
          {the password is the result of the random selection
           of a character from a constant named "charpool".}
+	 {'length' is a Pascal function that returns the size of an array}
 	 password := password + dictionary[random(length(dictionary))];
 	 clrscr;
 	 gotoxy(40,12);
@@ -73,34 +79,60 @@ end;
 begin
    clrscr;
    textbackground(black);
-   textcolor(green);
+   textcolor(yellow);
    gotoxy(40,12);
-   writeln('Welcome to password generator');
-   gotoxy(40,14);
+   write('Welcome to password generator');
+   gotoxy(40,13);
    delay(1000);
-   writeln('Press any key to continue...');
+   write('Press any key to continue...');
+   gotoxy(40,30);
    readkey;
    clrscr;
-   gotoxy(40,12);
-   write('Do you need numbers in your password? [Y/N]  ');
-   readln(number);
-   clrscr;
-   gotoxy(40,12);
-   write('Do you need special in your password? [Y/N]  ');
-   readln(special);
-   clrscr;
-   gotoxy(40,12);
-   write('How long is your password? ');
-   readln(plength);
-   { writeln('You selections were ', number, ' ',  special, ' ', plength); }
-   { delay(5000); }
-   clrscr;
+   gotoxy(40,29);
+
+   { TODO :: ADD A VERIFICATION USING 'UNTIL, REPEAT' STRUCTURE }
    
-   passwordgenerator(plength, number, special);
+   repeat
+      clrscr;
+      gotoxy(40,12);
+      write('Do you need numbers in your password? [Y/N]  ');
+      readln(number);
+   until (number = 'Y') or (number = 'y') or (number = 'N') or (number = 'n');
+
+   repeat
+      clrscr;
+      gotoxy(40,12);
+      write('Do you need special in your password? [Y/N]  ');
+      readln(special);
+   until (special = 'Y') or (special = 'y') or (special = 'N') or (special = 'n');
+
+   repeat
+      clrscr;
+      gotoxy(40,12);
+      write('How long is your password? ');
+      readln(plength);
+      for i := 1 to length(plength) do
+      begin
+	 { writeln('The current value for index ' , i, ' is: ', plength[i]); }
+	 isanumber := isnumber(plength[i]);
+	 if not (isanumber) then
+	 begin
+	    gotoxy(40,13);
+	    writeln('This is not a number!');
+	    delay(3000);
+	    break;
+	 end;
+      end;
+   until isanumber;
+   {Convert the string into a number an sends the value to a new variable}
+   val(plength, nlength, error);
+   writeln(nlength);
+	     	     
+   passwordgenerator(nlength, number, special);
 
    clrscr;
    gotoxy(40,12);
-   writeln('press any key to exit.');
+   writeln('Press any key to exit.');
    readkey;
    clrscr;
 
